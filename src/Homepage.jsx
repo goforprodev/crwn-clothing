a05c1a78e1a69da9041ea008c49bc18bca0a3b77
Prodev <goforprodev@gmail.com>
2022-01-17 14:47:59 +0100

    created projected with create-react-app

---
+++ src/Homepage.jsx
@@ -0,0 +1,51 @@
+// styles
+import './Homepage.scss'
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                {/* 1 */}
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                {/* 2 */}
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title' >HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                {/* 3 */}
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                {/* 4 */}
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Homepage
